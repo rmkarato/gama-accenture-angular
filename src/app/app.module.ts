4b73fc865f92926d80de9b6c30644849810c0219
Renata <rmkarato@gmail.com>
2021-02-19 20:48:27 -0300

    novo módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,19 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeBr from '@angular/common/locales/pt';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { DataBindingExerciseComponent } from './shared/components/data-binding-exercise/data-binding-exercise.component';
-import { ExercicioContadorComponent } from './shared/components/exercicio-contador/exercicio-contador.component';
-import { ExercicioDiretivasComponent } from './shared/components/exercicio-diretivas/exercicio-diretivas.component';
-import { ExerccioNgclassComponent } from './shared/components/exerccio-ngclass/exerccio-ngclass.component';
-import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from 'src/app-routing.module';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { SharedModule } from './shared/shared.module';
 
 registerLocaleData(localeBr, "pt");
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
-    DataBindingExerciseComponent,
-    ExercicioContadorComponent,
-    ExercicioDiretivasComponent,
-    ExerccioNgclassComponent,
-    ExercicioPipesComponent,
     LoginComponent,
     ExtratoComponent,
     NaoEncontradoComponent
@@ -36,6 +22,7 @@ registerLocaleData(localeBr, "pt");
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule
   ],
   providers: [{
     provide: LOCALE_ID,

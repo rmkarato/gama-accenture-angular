cdfcae829f8343b8058f46220a6fe42a840d950d
Renata <rmkarato@gmail.com>
2021-02-21 17:18:44 -0300

    adicionado serviços de autenticação

---
+++ src/app/shared/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+export interface User {
+    nome: string;
+    sobrenome: string;
+    email: string;
+} 
---
+++ src/app/shared/services/auth/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user: User;
+  token: string;
+
+  constructor() { }
+
+  setUser(user: User) {
+    this.user = user;
+    localStorage.setItem("user", JSON.stringify(user))
+  }
+
+  setToken(token: string) {
+    this.token = token;
+    localStorage.setItem("token", JSON.stringify(token))
+  }
+
+  getUser() {
+    if (this.user) {
+      return this.user;
+    }
+    const usuarioGuardado = localStorage.getItem("user");
+
+    if(usuarioGuardado) {
+      this.user = JSON.parse(usuarioGuardado)
+      return this.user
+    }
+    return null;
+  }
+
+  getToken() {
+    if (this.token) {
+      return this.token;
+    }
+    const tokenGuardado = localStorage.getItem("token");
+
+    if(tokenGuardado) {
+      this.token = JSON.parse(tokenGuardado)
+      return this.token
+    }
+    return null;
+  }
+}
